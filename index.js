2b83753224049390241362affb1a436f07153ab8
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-01 21:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase()===string){return "I can't hear you!";
   } if(string.toUpperCase()===string){return"YES INDEED!";
-  } if(string===)
+  } if(string==="I love you, Grandma."){return 
 }
